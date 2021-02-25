2e2f3006815ab627a483e6d499b9a2cc94949178
ElhourefAissam <aissamelhouref@gmail.com>
2021-02-25 14:17:48 +0100

    add pages

---
+++ resources/js/components/product/listing.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="(product, i) in getProducts" :key="i">{{ product.name }}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from "vuex";
+
+export default {
+    methods: {
+        ...mapActions(["fetchProducts"])
+    },
+    computed: {
+        ...mapGetters(["getProducts"])
+    },
+    created() {
+        this.fetchProducts();
+    }
+};
+</script>
+
+<style></style>
